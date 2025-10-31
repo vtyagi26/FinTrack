dab1305ca5d4d5f024e8dc0a0240a42549dcb834
vtyagi26 <vaibhavtyagi9981@gmail.com>
2025-10-31 09:51:22 +0530

    Deletions

--- backend/model/HoldingModel.js
+++
@@ -1,7 +0,0 @@
-const { model } = require("mongoose");
-
-const { HoldingSchema } = require("../schemas/HoldingSchema");
-
-const HoldingModel = new model("holding", HoldingSchema);
-
-module.exports = { HoldingModel };
--- backend/model/OrdersModel.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-const { OrdersSchema } = require("../schemas/OrdersSchema");
-
-const OrdersModel = mongoose.model("Order", OrdersSchema);
-
-module.exports = { OrdersModel };
