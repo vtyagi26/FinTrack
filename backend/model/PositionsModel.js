c02fc015125f3fd2322bf6e74a62a42942d2df6f
VAIBHAV TYAGI <118992838+vtyagi26@users.noreply.github.com>
2025-10-31 09:50:17 +0530

    Delete backend directory

--- backend/model/PositionsModel.js
+++
@@ -1,7 +0,0 @@
-const { model } = require("mongoose");
-
-const { PositionsSchema } = require("../schemas/PositionsSchema");
-
-const PositionsModel = new model("position", PositionsSchema);
-
-module.exports = { PositionsModel };
